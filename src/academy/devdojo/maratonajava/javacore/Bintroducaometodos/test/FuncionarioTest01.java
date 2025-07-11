c750be25e114935feec7b8476accc1e7a574e4d0
unknown <batatovisk0330@gmail.com>
2025-07-11 15:21:11 -0300

    Exercise of methods

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -9,7 +9,6 @@ public class FuncionarioTest01 {
         funcionario.idade = 18;
         funcionario.salarios = new double[]{2000.32, 1800.90, 2400.03};
 
-
         funcionario.imprimeDados();
         funcionario.temBonus();
     }
