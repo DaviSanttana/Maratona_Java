820d284b64b84f6baed22e1061c523fae61adb0a
unknown <batatovisk0330@gmail.com>
2025-07-21 13:53:14 -0300

    ACESS STATIC

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.test;
+
+import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("Lamborghini0", 312);
+        Carro c2 = new Carro("Bugatti", 330);
+        Carro c3 = new Carro("Mclaren", 320);
+        Carro.velocidadeLimite = 180;
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
+
+    }
+}
