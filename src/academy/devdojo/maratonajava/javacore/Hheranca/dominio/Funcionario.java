a258f1b1084aaa6181bb0dbc4d6261492f6ed8c6
unknown <batatovisk0330@gmail.com>
2025-08-07 19:55:16 -0300

    protected

--- src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -7,6 +7,9 @@ public class Funcionario extends Pessoa {
         super.imprime();
         System.out.println(this.salario);
     }
+    public void relatorioPagamento(){
+        System.out.println("Eu "+this.nome+ " recebi o salario de "+this.salario);
+    }
     public double getSalario() {
         return salario;
     }
