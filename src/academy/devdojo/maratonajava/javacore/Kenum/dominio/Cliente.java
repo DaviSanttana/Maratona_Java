d4ca654bf303f9995729b7c141395db65e852256
unknown <batatovisk0330@gmail.com>
2025-08-26 21:06:59 -0300

    ENUMERATION

--- src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
@@ -1,34 +1,21 @@
 package academy.devdojo.maratonajava.javacore.Kenum.dominio;
 
 public class Cliente {
+    public  enum TipoPagamento{
+        DEBITO,CREDITO
+    }
     private String nome;
     private TipoCliente tipoCliente;
+    private  TipoPagamento tipoPagamento;
 
-
-    public Cliente(String nome, TipoCliente tipoCliente) {
+    public Cliente(String nome, TipoCliente tipoCliente, TipoPagamento tipoPagamento) {
         this.nome = nome;
         this.tipoCliente = tipoCliente;
-
+        this.tipoPagamento = tipoPagamento;
     }
 
     @Override
     public String toString() {
-        return "Cliente: " + nome  + " | tipoCliente: " + tipoCliente;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public TipoCliente getTipoCliente() {
-        return tipoCliente;
-    }
-
-    public void setTipoCliente(TipoCliente tipoCliente) {
-        this.tipoCliente = tipoCliente;
+        return "Cliente: " + nome  + "  |  tipoCliente: " +tipoCliente.getNomeRelatorio() + "  |  tipoClienteInt: " +tipoCliente.VALOR+ " | " + " tipoPagamento: "+ tipoPagamento;
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
@@ -1,6 +1,22 @@
 package academy.devdojo.maratonajava.javacore.Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA,
+    PESSOA_FISICA(1,"Pessoa Fisica"),
+    PESSOA_JURIDICA(2,"Pessoa Juridica");
+
+
+    public final int VALOR;
+    public String nomeRelatorio;
+    TipoCliente(int  VALOR, String nomeRelatorio) {
+        this.VALOR = VALOR;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public int getVALOR() {
+        return VALOR;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
 }
