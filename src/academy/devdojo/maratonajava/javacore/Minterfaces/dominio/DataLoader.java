383ef0fe452823981eed9606a458b5d21d47b64a
Davi <batatovisk0330@gmail.com>
2025-09-08 22:50:40 -0300

    Aprendendo sobre o default

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
@@ -2,4 +2,7 @@ package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
 
 public interface DataLoader {
      public abstract void load();
+     default void checkPermission(){
+          System.out.println("Fazendo a checagem de permissões");
+     }
 }
