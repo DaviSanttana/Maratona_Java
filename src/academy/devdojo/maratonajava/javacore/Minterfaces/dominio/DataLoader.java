fa36f73161b6692f151339306589f6c94d7adab8
Davi <batatovisk0330@gmail.com>
2025-09-16 23:07:03 -0300

    Atributes and methods statics

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
@@ -1,8 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
 
 public interface DataLoader {
+     public static final int MAX_DATA_SIZE = 10 ;
      public abstract void load();
-     default void checkPermission(){
+     default void checkPermission() {
           System.out.println("Fazendo a checagem de permissões");
      }
-}
+               public static void retrieveMaxDataSize(){
+                    System.out.println("Dentro do retrieveMaxDataSize na interface");
+          }
+     }
+
--- src/academy/devdojo/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
@@ -1,5 +1,6 @@
 package academy.devdojo.maratonajava.javacore.Minterfaces.test;
 
+import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.DataLoader;
 import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.DatabaseLoader;
 import academy.devdojo.maratonajava.javacore.Minterfaces.dominio.FileLoader;
 
@@ -14,5 +15,9 @@ public class DataLoaderTest01 {
         fileLoader.remove();
         databaseLoader.checkPermission();
         fileLoader.checkPermission();
+
+
+        DatabaseLoader.retrieveMaxDataSize();
+        DataLoader.retrieveMaxDataSize();
     }
 }
