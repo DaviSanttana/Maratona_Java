4c0ce50ba5c775465162c9f926d83e76234e35b3
Davi <batatovisk0330@gmail.com>
2025-09-17 21:21:41 -0300

    91 - Orientation object Polymprphism pt02 - operation

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/dominio/Tomate.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/dominio/Tomate.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio;
 
 public class Tomate extends  Produto{
     public static final double IMPOSTO_POR_CENTO = 0.06;
+    private String dataValidade;
     public Tomate(String nome, double valor) {
         super(nome, valor);
     }
@@ -20,5 +21,13 @@ public class Tomate extends  Produto{
     @Override
     public double getValor() {
         return super.getValor();
+    }
+
+    public String getDataValidade() {
+        return dataValidade;
+    }
+
+    public void setDataValidade(String dataValidade) {
+        this.dataValidade = dataValidade;
     }
 }
