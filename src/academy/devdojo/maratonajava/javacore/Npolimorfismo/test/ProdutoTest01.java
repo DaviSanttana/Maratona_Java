c33162bf65b37cc275bc1177752e18852cc2430f
Davi <batatovisk0330@gmail.com>
2025-09-23 17:52:47 -0300

    92 - Object Orientation - Polymorphism pt 03 - Polymorphic Parameters

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
 
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,8 +9,13 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("Samsung g7",11000);
         Tomate tomate = new Tomate("Tomate anão",10);
+        Televisao tv = new Televisao("Samsung 48\"",3000);
 
-        CalculadoraImposto.CalculadoraImpostoComputador(computador);
-        CalculadoraImposto.CalculadoraImpostoTomate(tomate);
+        CalculadoraImposto.CalculadoraImposto(computador);
+        System.out.println("-----------------------");
+        CalculadoraImposto.CalculadoraImposto(tomate);
+        System.out.println("-----------------------");
+        CalculadoraImposto.CalculadoraImposto(tv);
+
     }
 }
