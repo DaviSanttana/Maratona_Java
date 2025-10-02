3da3abcc59ed87dbe212ee65cac7441c8c48b61d
Davi <batatovisk0330@gmail.com>
2025-10-01 22:39:22 -0300

    94 - Orientação Objetos - Polimorfismo pt 05 - Programação orientada a interface

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/RepositorioTeste.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/RepositorioTeste.java
@@ -5,13 +5,21 @@ import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioAr
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioBancoDeDados;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioMemoria;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RepositorioTeste {
     public static void main(String[] args) {
+        List<String>list = new ArrayList<>();
         Repositorio repositorio  = new RepositorioBancoDeDados();
         Repositorio repositorio2  = new RepositorioArquivo();
         Repositorio repositorio3  = new RepositorioMemoria();
         repositorio.salvar();
         repositorio2.salvar();
         repositorio3.salvar();
+        list.add("Davi");
+        list.add("Gabriel");
+        list.add("Santana");
+        System.out.println(list);
     }
 }
